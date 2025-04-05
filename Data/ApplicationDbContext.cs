2f7e3858756a56d6578d6e98ebda4e355d6d9a4f
Sadman-Ishtiak <sadmanishtiak1@gmail.com>
2025-04-05 13:15:39 +0600

    Identity

---
+++ Data/ApplicationDbContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace web_dev_assignment.Data;
+
+public class ApplicationDbContext : IdentityDbContext
+{
+    public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+        : base(options)
+    {
+    }
+}
--- Models/Item.cs
+++
@@ -1,9 +0,0 @@
-namespace web_dev_assignment.Models
-
-{
-    public class Item {
-        public int ID{ get; set; }
-        public string Name{ get; set;}
-        public string Description{ get; set;}
-    }
-}
