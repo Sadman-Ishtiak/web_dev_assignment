0f15ec537708a4be9172a3f8a4f13c0b3d239ead
Sadman-Ishtiak <sadmanishtiak1@gmail.com>
2025-04-11 13:00:33 +0600

    fear

--- Models/ApplicationUser.cs
+++ Models/ApplicationUser.cs
@@ -1,11 +1,7 @@
 using Microsoft.AspNetCore.Identity;
 
-namespace web_dev_assignment.Models
-{
-    public class ApplicationUser : IdentityUser
-    {
-        // Optional: Add user-specific info here
-
-        public Cart Cart { get; set; }  // 👈 Navigation to their Cart
+namespace web_dev_assignment.Models {
+    public class ApplicationUser : IdentityUser {
+        public Cart Cart { get; set; }
     }
 }
